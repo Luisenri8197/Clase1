bc52f03203b54436ec480a808a96e579308218ba
Luisenri8197 <luisenriqueordonez2008@gmail.com>
2026-03-10 22:11:26 -0500

    Se reconfigura ruta debugger

--- 02-datatypes.js
+++ 02-datatypes.js
@@ -1,2 +1,13 @@
 //datos basicos inmutables - representan un solo valor
 
+//String
+let cadenaTexto = "Soy un dato tipo string"
+//Numero
+let  num = 5 //Entero
+let fracc = 5.5 //decimal
+//Boolean
+let booleanoFalso = false
+let booleanoVerdadero = true
+//Undefined
+let indefinido
+console.log(indefinido)
