a1755b798969bfa5a536c509875426725b6df961
Luisenri8197 <luisenriqueordonez2008@gmail.com>
2026-03-10 22:26:12 -0500

    Terminando lso datatypes

--- 02-datatypes.js
+++ 02-datatypes.js
@@ -1,4 +1,5 @@
-//datos basicos inmutables - representan un solo valor
+//Datos basicos inmutables - representan un solo valor
+//Datos primitivos
 
 //String
 let cadenaTexto = "Soy un dato tipo string"
@@ -11,3 +12,24 @@ let booleanoVerdadero = true
 //Undefined
 let indefinido
 console.log(indefinido)
+//null
+let valorNull = null
+console.log(valorNull)
+//Symbol
+let mySymbol = Symbol("mysymbol")
+//Bigint
+let myBigInt1 = BigInt(8464651651618899816516516511984894312191)
+let myBigInt12 = 65165165132165168168151315189489497984894n
+
+//Validar tipo de datos
+
+console.log(typeof(cadenaTexto))
+console.log(typeof(num))
+console.log(typeof(fracc))
+console.log(typeof(booleanoFalso))
+console.log(typeof(booleanoVerdadero))
+console.log(typeof(indefinido))
+console.log(typeof(valorNull))
+console.log(typeof(mySymbol))
+console.log(typeof(myBigInt1))
+console.log(typeof(myBigInt12))
